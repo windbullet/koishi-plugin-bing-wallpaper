67e1a907933b71b3c8403ad56a4341a81a699dab
windbullet <2536810643@qq.com>
2025-01-13 10:42:41 +0800

    提交文件

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Context, Schema } from 'koishi'
+import { Context, Schema, h } from 'koishi'
 
 export const name = 'bing-wallpaper'
 
@@ -7,5 +7,25 @@ export interface Config {}
 export const Config: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context) {
-  // write your plugin here
+  ctx.command("必应壁纸")
+
+  ctx.command("必应壁纸.768p")
+    .action(({session}) => {
+      return h.image("https://bing.img.run/1366x768.php")
+    })
+
+  ctx.command("必应壁纸.1080p")
+    .action(({session}) => {
+      return h.image("https://bing.img.run/1920x1080.php")
+    })
+
+  ctx.command("必应壁纸.4k")
+    .action(({session}) => {
+      return h.image("https://bing.img.run/uhd.php")
+    })
+
+  ctx.command("必应壁纸.手机")
+    .action(({session}) => {
+      return h.image("https://bing.img.run/m.php")
+    })
 }
